c8bc68b2c804a613198b46955c1871f100a76108
platformPMM <edquek@hotmail.com>
2018-02-12 16:18:29 +0800

    Initial Commit

---
+++ EdCons/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EdCons
+//
+//  Created by Edmund Quek on 2/12/18.
+//  Copyright © 2018 Edmund Quek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
